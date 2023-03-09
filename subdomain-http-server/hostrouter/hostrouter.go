4c3956ff4044ce47b944f57d7bdd3cfec23b5204
Sheldon Lobo <sheldon.lobo@pendo.io>
2023-03-08 21:28:49 -0500

    More logging for subdomain http server

--- subdomain-http-server/hostrouter/hostrouter.go
+++ subdomain-http-server/hostrouter/hostrouter.go
@@ -1,6 +1,7 @@
 package hostrouter
 
 import (
+	"log"
 	"net/http"
 	"strings"
 
@@ -28,6 +29,9 @@ func (hr Routes) Unmap(host string) {
 }
 
 func (hr Routes) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Request: %s %s/%s?%s#%s ; From: %s", r.Method, r.Host, r.URL.Path,
+		r.URL.RawQuery, r.URL.RawFragment, r.RemoteAddr)
+
 	host := requestHost(r)
 	if router, ok := hr[strings.ToLower(host)]; ok {
 		router.ServeHTTP(w, r)
