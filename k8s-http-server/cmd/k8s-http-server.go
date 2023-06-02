11fbd067758259ba85b58576039d574e0fd399ee
Sheldon Lobo <sheldon.lobo@pendo.io>
2023-06-02 15:48:29 -0400

    k8s-http-server : Try graph

--- k8s-http-server/cmd/k8s-http-server.go
+++ k8s-http-server/cmd/k8s-http-server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/go-chi/cors"
 	"github.com/gocql/gocql"
 	"html/template"
 	"log"
@@ -92,16 +93,16 @@ func main() {
 	// Setup chi with hostrouter
 	router := chi.NewRouter()
 
-	//router.Use(cors.Handler(cors.Options{
-	//	// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-	//	AllowedOrigins: []string{"https://*", "http://*"},
-	//	// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-	//	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-	//	AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-	//	ExposedHeaders:   []string{"Link"},
-	//	AllowCredentials: false,
-	//	MaxAge:           300, // Maximum value not ignored by any of major browsers
-	//}))
+	router.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	hostRouter := hostrouter.New()
 
