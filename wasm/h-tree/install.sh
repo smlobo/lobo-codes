9ac57417083412289b7129d6c4453fa14495ad59
Sheldon Lobo <sheldon.lobo@oracle.com>
2024-07-17 00:35:10 -0400

    Fix h-tree install script - do not create 'install' dir

--- wasm/h-tree/install.sh
+++ wasm/h-tree/install.sh
@@ -6,7 +6,6 @@ PROJECT_NAME="h-tree"
 
 BUILD_DIR=${1}
 
-mkdir install
 cp ${BUILD_DIR}/${PROJECT_NAME}.js .
 cp ${BUILD_DIR}/${PROJECT_NAME}.wasm .
 cp ${BUILD_DIR}/${PROJECT_NAME}.html index.html
